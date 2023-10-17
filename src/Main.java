17993c86b13bdbe1270444e81352ff98ef952058
DESKTOP-UIEJSCU\oTriks <maar_tiin@hotmail.com>
2023-10-17 09:24:38 +0200

    added comment

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("Hello world!");
+        System.out.println("Hello world!");  // här skriver vi ut något på skärmen 
     }
 }
